552d682f66a45989486386c5d785f6fa3dbb2fcd
JasleenKaur2000 <jasleenkaur46353@gmail.com>
2021-07-31 10:39:35 +0530

    created top component of chat room

--- src/components/chat-rooms/top/index.js
+++ src/components/chat-rooms/top/index.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { memo } from 'react';
+import { Link } from 'react-router-dom';
+import { ButtonToolbar, Icon } from 'rsuite';
 import { useCurrentRoom } from '../../../context/current-room.context';
+import { useMediaQuery } from '../../../misc/custom-hooks';
+import roomInfoBtnModal from './roomInfoBtnModal';
 
 const ChatTop = () => {
-  const { name } = useCurrentRoom(v => v.name);
-  return <div>{name}</div>;
+  const name = useCurrentRoom(v => v.name);
+  const isMobile = useMediaQuery(`(max-width:992px)`);
+
+  return (
+    <div>
+      <div className="d-flex justify-content-between align-items-center">
+        <h4>
+          <Icon
+            componentClass={Link}
+            icon="arrow-circle-left"
+            size="2x"
+            className={
+              isMobile
+                ? 'd-inline-block p-0 mr-2 text-blue link-unstyled'
+                : 'd-n-ne'
+            }
+          />
+          <span className="text-disappear">{name}</span>
+        </h4>
+        <ButtonToolbar className="ws-no-wrap">Todo</ButtonToolbar>
+      </div>
+      <div className="d-flex justify-content-between align-items-center">
+        <span>todo</span>
+        <roomInfoBtnModal />
+      </div>
+    </div>
+  );
 };
 
-export default ChatTop;
+export default memo(ChatTop);
